1b4107c301474f9e9e734bbd01210a61c979ea34
Kamil Tylka <kamil.tylka@hydrogentech.pl>
2024-11-20 11:33:04 +0100

    Test widgets existence added

--- tests/test_app.py
+++ tests/test_app.py
@@ -18,3 +18,17 @@ def test_frame_existence(app):
     assert isinstance(app.amount_of_dfc, tk.Frame)
     assert isinstance(app.top_bot, tk.Frame)
     assert isinstance(app.buttons, tk.Frame)
+    
+def test_widgets_existence(app):
+    assert isinstance(app.dfc_name_label, tk.Label)
+    assert isinstance(app.dfc_name_entry, tk.Entry)
+    
+    assert isinstance(app.dfc_amount, tk.Label)
+    assert isinstance(app.spinbox_amount, tk.Spinbox)
+    
+    assert isinstance(app.top, tk.Checkbutton)
+    assert isinstance(app.bot, tk.Checkbutton)
+
+    assert isinstance(app.map, tk.Button)
+    assert isinstance(app.milling, tk.Button)
+    assert isinstance(app.position, tk.Button)
